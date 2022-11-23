e8fe9af9c29d5227a6043d9e2b5d91c6cfc5e174
Sun <78437222+SungmanKang@users.noreply.github.com>
2022-11-23 02:03:54 -0600

    Update bertram.py
    Fix the error in the add_word_vectors_to_model function.
    runtimeerror: a view of a leaf variable that requires grad is being used in an in-place operation.
    --> with torch.no_grad():

--- BERTRAM/bertram.py
+++ BERTRAM/bertram.py
@@ -481,8 +481,11 @@ class BertramWrapper:
         # add the word vectors for all words to the model's embedding matrix
         for word, embedding in embeddings.items():
             word_id = tokenizer.convert_tokens_to_ids(f"<BERTRAM:{word}>")
-            _get_embeddings_module(model).word_embeddings.weight[word_id] = embedding
-
+            
+            #Modify by SM (20221123)
+            torch.tensor(embedding, requires_grad = True)    
+            with torch.no_grad():
+                _get_embeddings_module(model).word_embeddings.weight[word_id] = embedding
 
 def _get_embeddings_module(model: PreTrainedModel):
     if hasattr(model, 'bert'):
